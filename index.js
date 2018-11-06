a885b3caae15485b8435bdd76e1d7a64ed303bce
Guy Moses <guymos@microsoft.com>
2018-11-06 16:57:43 +0200

    The package returns correct API version without patch number
    Increase package version.
    Check schemas in test

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-exports.version = require('./package.json').version;
+const semver = require('semver');
+
+let packageVersion = require('./package.json').version;
+let apiVersion = `${semver.major(packageVersion)}.${semver.minor(packageVersion)}.0`;
+exports.version = apiVersion;
 
 exports.schemas = {
     capabilities: require(`./schema.capabilities.json`),
