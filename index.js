5003b9fa12090d1b7da6f2c1f40c19d829b58aeb
Ignat Vilesov <ignat.vilesov@gmail.com>
2018-11-08 11:06:30 +0200

    29254 Update schema: Power BI Visual API should support fontFamily (#6)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const semver = require('semver');
 
 let packageVersion = require('./package.json').version;
 let apiVersion = `${semver.major(packageVersion)}.${semver.minor(packageVersion)}.0`;
+
 exports.version = apiVersion;
 
 exports.schemas = {
