f6823a949b150565df19fe35ec2257452322f199
Sherein Dabbah <sdabbah@microsoft.com>
2022-06-21 11:30:54 +0300

    Revert index.d.ts file from commit "Bump to version 5.0.0"
    This reverts commit 9e00af23790cb5f70988145a3913730d847b97c2.

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-// Powerbi visuals APIs
-/// <reference path='./src/visuals-api.d.ts'/>
-
-// Powerbi formatting pane APIs
-/// <reference path='./src/formatting-model-api.d.ts'/>
-
-export default powerbi;
