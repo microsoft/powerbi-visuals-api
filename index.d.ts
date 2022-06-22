c5639a166fdc2a6c59da40309f53413de8e6b8d5
Sherein Dabbah <sdabbah@microsoft.com>
2022-06-22 09:37:32 +0300

    Add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+// Powerbi visuals APIs
+/// <reference path='./src/visuals-api.d.ts'/>
+
+// Powerbi formatting pane APIs
+/// <reference path='./src/formatting-model-api.d.ts'/>
+
+export default powerbi; 
